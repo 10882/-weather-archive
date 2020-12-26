7026ae2afcefea5330aace667b999baacf54edd1
10882 <62587713+10882@users.noreply.github.com>
2020-12-26 17:40:32 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+apikey = ''
+#Enter weather longitude
+lon = 0
+#Enter weather latitude
+lat = 0
+#Enter addres of you mysql server
+mysql_server_addres = ''
+#Enter mysql server user
+mysql_server_user = ''
+#Enter mysql server password
+mysql_server_pass = ''
+#Enter name of mysql database 
+db = ''
